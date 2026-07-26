96fd67b245b79a35e99e2649e2c1f494412de782
Pingping Wang <152523426+Pingping9@users.noreply.github.com>
2026-07-26 18:56:16 -0500

    Update __init__.py

--- PyGALAX/__init__.py
+++ PyGALAX/__init__.py
@@ -3,7 +3,7 @@ __version__ = "0.1.0"
 from .kernel import Kernel
 from .bandwidth import check_class_sizes, search_bw_lw_ISA, search_bandwidth
 from .model import GALAX
-from .results import GALAXResults
+from .results import GALAXResults, LocalHoldoutResults
 
 __all__ = [
     "Kernel",
@@ -12,6 +12,7 @@ __all__ = [
     "search_bandwidth",
     "GALAX",
     "GALAXResults",
+    "LocalHoldoutResults",
 ]
 
 
